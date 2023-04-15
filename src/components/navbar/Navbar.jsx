5c63a97dc9a9230fab713e56a76de8ed7898427a
Shivansh Sharma <shivanshsharma8@gmail.com>
2023-04-15 16:48:31 -0400

    Chkpt 2: navbar scroll

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import "./navbar.scss"
 import { ArrowDropDown, Notifications, Search } from '@mui/icons-material'
-const Navbar = () => {
-  return (
-    <div className='navbar'>
+
+const Navbar = (props) => {
+    // Creating a useState hook to store the state of scroll
+    const {isScrolled, setIsScrolled} = props;
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);  // Preventing infinite loop
+    }
+
+    return (
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
         <div className="container">
             <div className="left">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="" />
@@ -18,7 +27,15 @@ const Navbar = () => {
                 <span>KID</span>
                 <Notifications className='icon'/>
                 <img src="https://raw.githubusercontent.com/shiva2096/shiva2096.github.io/master/assets/img/profile-img.jpg" alt="" />
-                <ArrowDropDown className='icon'/>
+                
+                <div className="profile">
+                    <ArrowDropDown className='icon'/>
+                    <div className="options">
+                        <span>Settings</span>
+                        <span>Logout</span>
+                    </div>
+                </div>
+                
             </div>
         </div>
     </div>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./home.scss"
 import Navbar from '../components/navbar/Navbar'
 
 const Home = () => {
-  return (
+    const [isScrolled, setIsScrolled] = useState(false);  // If you make it inside Navbar, then it state change function runs twice
+
+    return (
     <div className='home'>
-        <Navbar/>
+        <Navbar isScrolled={isScrolled} setIsScrolled={setIsScrolled}/>
+
+        <img id="homeImage" src="/images/home.jpeg" alt="" />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit repellat, assumenda neque officiis consequatur doloribus maxime ipsam amet earum, fugit, dolores in! Dolor error adipisci similique iure in ea reprehenderit.
     </div>
   )
 }
